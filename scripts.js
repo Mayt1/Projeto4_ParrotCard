db372160d4fe1f9008163e74943fa5ee7446ebe0
VictorUlian <yctor.ulian@gmail.com>
2022-02-07 09:31:11 -0300

    Este commit irá adicionar os arquivos iniciais do jogo da memoria

---
+++ scripts.js
@@ -0,0 +1,9 @@
+minhaArray.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+
